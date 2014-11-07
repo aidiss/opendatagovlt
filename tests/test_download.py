2b1df67ab7beb6822b77d034ee4575db7cb0d574
Aidis Stukas <aidiss@gmail.com>
2014-11-07 12:10:22 +0200

    Revert "Revert "Alternative flow and colander""
    This reverts commit dbd3dfcf8dbe2272abee7bf0e9659ea033a5f996.

--- tests/test_download.py
+++ tests/test_download.py
@@ -11,3 +11,7 @@ def response_content(url, request):
 @httmock.with_httmock(response_content)
 def test_download():
     opendatagovlt.download_pages()
+
+
+def test_download_page_alt0(number):
+    opendatagovlt.download_alt0()
