c5db3112d9f42c6dcd7932c31d8faeb26707dc89
Aidis Stukas <aidiss@gmail.com>
2014-11-26 13:10:44 +0200

    Atnaujintas duomenų validavimas.

--- data_scheme.py
+++ data_scheme.py
@@ -5,19 +5,19 @@ import datetime
 
 class Resource(colander.MappingSchema):
     code = colander.SchemaNode(colander.Int(), validator=colander.Range(0, 1000))
-    name = colander.SchemaNode(colander.String())
-    alt_name = colander.SchemaNode(colander.String())
-    description = colander.SchemaNode(colander.String())
-    keywords = colander.SchemaNode(colander.SequenceSchema) # limited choice?
-    tvarkytojas = colander.SchemaNode(colander.String())
-    contacts = colander.SchemaNode(colander.SequenceSchema)
-    data_type = colander.SchemaNode(colander.String()) 
-    data_format = colander.SchemaNode(colander.String())
-    start_of_rinkmena = colander.SchemaNode(colander.Date()) # datetime ?
-    end_of_rinkmena = colander.SchemaNode(colander.Date()) # datetime ?
-    update_frequency = colander.SchemaNode(colander.String()) # limited choice?
-    internet_address = colander.SchemaNode(colander.String()) # validator=colander.url ? does not work 
+    name = colander.SchemaNode(colander.String(), validator=colander.Length(min=5, max=20))
+    alt_name = colander.SchemaNode(colander.String(), validator=colander.Length(min=5, max=20))
+    description = colander.SchemaNode(colander.String(), validator=colander.Length(min=5, max=500))
+    keywords = colander.SchemaNode(colander.Sequence(), validator=colander.Length(min=5, max=20))
+    tvarkytojas = colander.SchemaNode(colander.String(), validator=colander.Length(min=5, max=20))
+    contacts = colander.SchemaNode(colander.Sequence(), validator=colander.Length(min=5, max=20))
+    data_type = colander.SchemaNode(colander.String(), validator=colander.Length(min=5, max=20))
+    data_format = colander.SchemaNode(colander.String(), validator=colander.Length(min=5, max=20))
+    start_of_rinkmena = colander.SchemaNode(colander.Date())  # datetime and string are other choice.
+    end_of_rinkmena = colander.SchemaNode(colander.Date())  # datetime and string are other choice.
+    update_frequency = colander.SchemaNode(colander.Tuple())  # tuple (times, per_time_unit
+    internet_address = colander.SchemaNode(colander.String())  # validator=colander.url ? does not work
     teikimo_salygos = colander.SchemaNode(colander.String())
-    data_reliability = colander.SchemaNode(colander.String()) # limited choice?
-    data_depth = colander.SchemaNode(colander.String()) # limited choice?
+    data_reliability = colander.SchemaNode(colander.Int(), validator=colander.Range(0, 5))
+    data_depth = colander.SchemaNode(colander.String(), validator=colander.Range(0, 5))
     rinkmenos_aprasymo_publikavimo_duomenys = colander.SchemaNode(colander.String())
